0c56bca8a4b0bdd4d43fe85edaefd4e181a2ce94
Alicia Hurst <alicia@aliciahurst.com>
2015-04-10 13:45:44 -0400

    Add social media ACF repeater in footer

--- footer.php
+++ footer.php
@@ -12,21 +12,36 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="wrapper">
-			<div class="site-info">
 				<div class="copyright">
 					<p>Alicia Hurst</p>
 					<p>Web Design &amp; Development</p>
 					<p><a href="mailto:alicia@aliciahurst.com">alicia@aliciahurst.com</a></p>
 					<p>&copy; <?php echo date('Y'); ?> Alicia Hurst LLC. All rights reserved.</p>
 				</div>
-				
+
+				<?php if( have_rows('social_media', 'option') ): ?>
+					<nav id="social-navigation-footer" class="social-navigation" role="navigation">
+						<ul>
+						<?php while( have_rows('social_media', 'option') ): the_row(); 
+
+							// vars
+							$network = get_sub_field('social_media_network');
+							$account = get_sub_field('social_media_account');
+
+							?>
+							<li><a href="http://<?php echo $network; ?>.com/<?php echo $account; ?>"><?php echo $network; ?></a></li>
+						<?php endwhile; ?>
+						</ul>
+					</nav>
+				<?php endif; ?>
+
 				<nav id="footer-navigation" class="secondary-navigation" role="navigation">
 					<ul>
 						<li><a href="/terms">Terms &amp; Privacy</a></li>
 						<li><a href="/credits">Credits</a></li>
 					</ul>
 				</nav>
-			</div><!-- .site-info -->
+
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
