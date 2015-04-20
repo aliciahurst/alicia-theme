239a1b1127a6369abcaa92fb9f8942278160b0d3
Alicia Hurst <alicia@aliciahurst.com>
2015-04-20 12:16:09 -0400

    Style footer

--- content-work.php
+++ content-work.php
@@ -7,4 +7,4 @@
 <div class="work-item">
 		<a href=" <?php the_permalink(); ?>"><img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/<?php echo $post->post_name;?>-thumb.jpg"></a>
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-	</div>
+</div>
--- footer.php
+++ footer.php
@@ -11,28 +11,23 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="wrapper">		
-				<div class="copyright">
-					<p><span class="copy">&copy; <?php echo date('Y'); ?> <a href="/">Alicia Hurst</a>. All rights reserved.</span> <span class="spacer">|</span> <a href="/terms">Terms &amp; Privacy</a> <span class="spacer">|</span> <a href="/credits">Credits</a>
-				</div>
-
+		<div class="wrapper">
+				<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="/"><strong>Alicia Hurst</strong></a>. All rights reserved.<span class="footer-links"><a href="/terms">Terms &amp; Privacy</a> <span class="spacer">|</span> <a href="/credits">Credits</a></span></p>
 				<?php if( have_rows('social_media', 'option') ): ?>
 					<nav id="social-navigation-footer" class="social-navigation" role="navigation">
-						<h2>Keep in Touch</h2>
 						<ul>
 						<?php while( have_rows('social_media', 'option') ): the_row(); 
-
-							// vars
 							$network = get_sub_field('social_media_network');
 							$account = get_sub_field('social_media_account');
-
 							?>
-							<li><a href="http://<?php echo $network; ?>.com/<?php echo $account; ?>"><svg class="icon-<?php echo $network; ?>"><use xlink:href="#icon-<?php echo $network; ?>"></use></svg></a></li>
+							<li>
+								<a href="http://<?php echo $network; ?>.com/<?php echo $account; ?>"><svg class="icon-<?php echo $network; ?>"><use xlink:href="#icon-<?php echo $network; ?>"></use></svg></a>
+							</li>
 						<?php endwhile; ?>
 						</ul>
 					</nav>
 				<?php endif; ?>	
-
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
