1ad758db70ff00eb024c57f68bb6ec35c53e4fa6
Alicia Hurst <alicia@aliciahurst.com>
2015-04-21 11:24:58 -0400

    Work thumbnails styles

--- content-work.php
+++ content-work.php
@@ -5,6 +5,8 @@
 ?>
 
 <div class="work-item">
-		<a href=" <?php the_permalink(); ?>"><img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/<?php echo $post->post_name;?>-thumb.jpg"></a>
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<a href=" <?php the_permalink(); ?>">
+			<img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/<?php echo $post->post_name;?>-thumb.jpg">
+			<h1><span><?php the_title(); ?></span></h1>
+		</a>
 </div>
