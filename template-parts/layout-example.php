552e878fb9771fafeb3d4b66d3b677884040605a
Alicia Hurst <alicia@aliciahurst.com>
2015-05-06 11:03:38 -0400

    Add example for Lara

---
+++ template-parts/layout-example.php
@@ -0,0 +1,60 @@
+
+  	
+  		<section class="layout-text">
+  			<p>Paragraph text!</p>
+  		</section>
+
+  	
+  
+  	  		  		  			
+  			<section class="display_slider">
+
+  				  					<ul class="rslides">
+  						  							<li><img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/slide1.jpg" alt="Cowboy Bear Ninja | Alicia Hurst"></li>
+  						  							<li><img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/slide2.jpg" alt="Cowboy Bear Ninja | Alicia Hurst"></li>
+  						  							<li><img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/slide3.jpg" alt="Cowboy Bear Ninja | Alicia Hurst"></li>
+  						  					</ul>
+
+  				
+  				  					<div class="stickem">
+  						<p>Test test!</p>
+  					</div>
+  				
+  			</section>
+
+
+  		
+  	
+  
+  	  		  		  			
+  			<section class="display_images">
+
+  				  					  						<img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/image1.jpg" alt="Cowboy Bear Ninja | Alicia Hurst">
+  					  						<img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/image2.jpg" alt="Cowboy Bear Ninja | Alicia Hurst">
+  					  				
+  				
+  			</section>
+
+
+  		
+  	
+  
+  	
+  		<section class="layout-text">
+  			<p>More text!</p>
+  		</section>
+
+  	
+  
+  	  		  		  			
+  			<section class="display_before">
+
+  				  					<div class="twentytwenty-container">
+  						  							<img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/before.jpg" alt="Cowboy Bear Ninja before and after | Alicia Hurst">
+  						  							<img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/after.jpg" alt="Cowboy Bear Ninja before and after | Alicia Hurst">
+  						  					</div>
+
+  				
+  				
+  			</section>
+
