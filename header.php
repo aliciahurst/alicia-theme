9317d2f7697ad403da220f2121196b4b29fb6e4a
Alicia Hurst <alicia@aliciahurst.com>
2015-06-01 10:29:38 -0400

    Add GA code

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 </head>
 
 <body <?php body_class($post->post_name); ?>>
+<?php get_template_part('template-parts/content', 'analytics'); ?>
 	<div id="page" class="hfeed site main animsition">
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'alicia-theme' ); ?></a>
 		<header id="masthead" class="site-header" role="banner">
