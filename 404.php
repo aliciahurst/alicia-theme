d9251a1403e0e66e55fb0af2ac7b65eaf68ed1c9
Alicia Hurst <alicia@aliciahurst.com>
2015-06-01 10:25:25 -0400

    Reindent 404 page

--- 404.php
+++ 404.php
@@ -1,38 +1,33 @@
 <?php
 /**
- * The template for displaying 404 pages (not found).
- *
- * @package alicia-theme
- */
+* The template for displaying 404 pages (not found).
+*
+* @package alicia-theme
+*/
 
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
-<section class="home-top">
-	<div class="home-title">
-		<h1 class="entry-title">404 &mdash; Page Not Found</h1>
-<p> Well that was unexpected! There's nothing here!
-						Maybe something used to be here, but it is no longer? We may never know.</p>
-						<p>Please click the back button on your browswer
-						and try another link. You can also check the address bar for common misspellings.</p>
-	</div>
+	<section class="home-top">
+		<div class="home-title">
+			<h1 class="entry-title">404 &mdash; Page Not Found</h1>
+			<p>There's nothing here!
+				Maybe something used to be, but it is no longer? We may never know.</p>
+				<p>Please click the back button on your browswer
+					and try another link. You can also check the address bar for common misspellings.</p>
+				</div>
 
-	<div class="rslides_container">
-		<ul id="home-slider" class="rslides">
-			<li>
-			<h3>&times;</h3>
-			<h2>If you gaze into the abyss, the abyss gazes also into you.</h2>
-					<h4>&mdash; Friedrich Nietzsche</h4>
-				<h3>&times;</h3>
-			</li>
-	</ul>
-</div>
-</section>
+				<div class="rslides_container">
+					<ul id="home-slider" class="rslides">
+						<li>
+							<h3>&times;</h3>
+							<h2>If you gaze into the abyss, the abyss gazes also into you.</h2>
+							<h4>Friedrich Nietzsche</h4>
+							<h3>&times;</h3>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</main><!-- #main -->
 
-
-
-</main><!-- #main -->
-
-
-<?php get_footer(); ?>
-
+		<?php get_footer(); ?>
