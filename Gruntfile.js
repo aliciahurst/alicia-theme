8c43266f48cc73f03f40acd146070e8f8065330a
Alicia Hurst <alicia@aliciahurst.com>
2015-05-31 22:13:37 -0400

    Remove image minifiers

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,37 +34,6 @@ module.exports = function(grunt) {
           }
         },
 
-        imagemin: {
-            dynamic: {                         // Dynamic target
-              files: [{
-                expand: true,                  // Enable dynamic expansion
-                cwd: 'assets/img/raw/',               // Src matches are relative to this path
-                src: ['*.{png,jpg,gif}'],       // Actual patterns to match
-                dest: 'assets/img/min/'               // Destination path prefix
-              }]
-            }
-        },
-
-        svgmin: {
-            options: {
-                plugins: [
-                    {
-                        removeViewBox: false
-                    }, {
-                        removeUselessStrokeAndFill: false
-                    }
-                ]
-            },
-            
-            dist: {
-                expand: true,
-                cwd: 'assets/img/svg-raw',
-                src: ['*.svg'],
-                dest: 'assets/img/svg-min',
-                ext: '.min.svg'
-            }
-        },
-
         watch: {
             scripts: {
                 files: ['assets/js/**/*.js', 'assets/**/*.scss'],
@@ -81,44 +50,20 @@ module.exports = function(grunt) {
                     spawn: false,
                 }
             },
-            images: {
-              files: ['assets/img/raw/*.{png,jpg,gif}'],
-              tasks: ['imagemin'],
-              options: {
-                    spawn: false,
-                }
-            },
-            livereload: {
-                // Browser live reloading
-                // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
-                options: {
-                  livereload: true
-                },
-                files: [
-                    'assets/css/main.css',
-                    'assets/js/build/scripts.js',
-                ]
-            }
+
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-svgmin');
     
     // Register tasks
     grunt.registerTask('default', [
         'sass',
         'concat',
         'jshint',
-        'imagemin',
-        'svgmin'
       ]);
 
-    grunt.registerTask('svgs', [
-        'svgmin',
-    ]);
 };
