0b8d6bef95adadf78b5633653f6e0c5553e11d76
Alicia Hurst <alicia@aliciahurst.com>
2015-07-18 19:08:47 -0400

    Switch from h2 to h3

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -5,68 +5,11 @@
  * @package alicia-theme
  */
 ?>
-<!-- <section class="home-top">
-	<div class="home-title">
-		<img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/design.png"> <h2>&plus;</h2><img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/development.png">
-		<h3>By Me, Alicia</h3>
-	</div>
-
-	<div class="rslides_container">
-		<ul id="home-slider" class="rslides">
-			<?php while ( have_rows('home_quotes') ) : the_row();
-			$quote = get_sub_field('single_quote' );
-			$author = get_sub_field('single_author' );
-			?>
-			<li>
-			<h3>&times;</h3>
-			<h2><?php echo $quote; ?></h2> 
-				<h4><?php echo $author; ?></h4>
-				<h3>&times;</h3>
-			</li>
-		<?php endwhile; ?>
-	</ul>
-</div>
-</section>
-
-<section class="home-services">
-	<h2>What I Do</h2>
-	<div class="services-container">
-		<?php while ( have_rows('services') ) : the_row(); ?>
-			<?php if( get_row_layout() == 'service_section' ): ?>
-				<div class="services-div fadeInUp">
-					<h3> <?php if( get_sub_field('service_svg') ): ?>
-						<?php $svg = get_sub_field('service_svg'); ?>
-						<svg class="icon-<?php echo $svg ?>"><use xlink:href="#icon-<?php echo $svg ?>"></use></svg>
-					<?php endif; ?>
-					<?php the_sub_field('service_section_title'); ?></h3>
-					<ul>
-						<?php while( have_rows('services_list') ): the_row(); ?>
-							<li><?php echo get_sub_field('service_item'); ?></li>
-						<?php endwhile; ?>
-					</ul>
-				</div>
-			<?php endif; ?>
-		<?php endwhile ; ?>
-	</div>
-</section>
-
-<section class="home-contact">
-		<div>
-			<h3>Projects</h3>
-			<p>I'm accepting inquiries for new, freelance front-end development projects, preferably those that are team-based.</p>
-			<p><a href="/contact" class="btn">Let's talk!</a></p>
-		</div>
-		<div>
-			<h3>Hire Me</h3>
-			<p>I’m available for a <strong>full-time, remote</strong> position as Product Manager or Front-End Developer on a web/SaaS applications team.</p>
-			<p><a href="http://linkedin.com/in/aliciahurst" class="no-line">Find me on LinkedIn</a> or <a href="mailto:hi@aliciahurst.com">Get in touch &rarr;</a></p>
-		</div>
-</section> -->
 
 <section id="about" class="home-about">	
 	<div class="bio-text">
 		<img src="/wordpress/wp-content/themes/alicia-theme/assets/img/min/logoname.png">
-		<h2>Product Manager at <a href="http://powertofly.com">PowerToFly</a></h2>
+		<h3>Product Manager at <a href="http://powertofly.com">PowerToFly</a></h3>
 		<nav id="social-navigation-footer" class="social-navigation" role="navigation">
 				<?php if( have_rows('social_media', 'option') ): ?>
 					<ul>
