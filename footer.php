321ac68d5d127791ebe5b310f5c049bb54c5deef
Alicia Hurst <alicia@aliciahurst.com>
2016-06-13 21:48:59 -0400

    removed the whole footer element from being commented out

--- footer.php
+++ footer.php
@@ -8,33 +8,7 @@
  */
 ?>
 
-<!-- 
-<footer id="colophon" class="site-footer" role="contentinfo">
-	
-	<div class="footer-container">
 
-		<div class="footer-center">
-			<p class="top"><a href="#top"><svg class="icon-double-up"><use xlink:href="#icon-double-up"></use></svg></a></p>
-
-
-			<nav id="social-navigation-footer" class="social-navigation" role="navigation">
-				<?php if( have_rows('social_media', 'option') ): ?>
-					<ul>
-						<li> <a href="mailto:hi@aliciahurst.com" class="animated"><svg class="icon-mail"><use xlink:href="#icon-mail"></use></svg></a> </li>
-						<?php while( have_rows('social_media', 'option') ): the_row(); 
-						$network = get_sub_field('social_media_network');
-						$account = get_sub_field('social_media_account');
-						?>
-						<li> <a href="http://<?php echo $network; ?>.com/<?php echo $account; ?>" class="animated"><svg class="icon-<?php echo $network; ?>"><use xlink:href="#icon-<?php echo $network; ?>"></use></svg></a> </li>
-					<?php endwhile; ?>
-				</ul>
-			<?php endif; ?>	
-		</nav> 
-
-		<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="/"><strong>Alicia Hurst</strong></a>, all rights reserved.</p>
-	</div>
-</div>
-</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
